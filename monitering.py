7566deb1dd154435e7ea46545778a62b4cfd23d3
adminprogrammer222 <108526611+adminprogrammer222@users.noreply.github.com>
2022-07-01 17:10:31 +0530

    Add files via upload

---
+++ monitering.py
@@ -0,0 +1,19 @@
+import pygame 
+import psutil as ps 
+import  sys
+pygame.init()
+theFont=pygame.font.Font(None,72)
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode([320, 150])
+pygame.display.set_caption('Live Cpu Usage')
+def text(Fnt,size,txt,clr,corx,cory):
+    font = pygame.font.Font(Fnt,size)
+    txt = font.render(txt,True,clr)
+    screen.blit(txt,(corx,cory))
+while True:
+    screen.fill((0,0,0))
+    text('TarrgetAcademyRegular-ABEx.otf',30,str(ps.cpu_percent(interval=0.200)),(192,192,192),100,50)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
